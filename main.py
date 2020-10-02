6cc495b99be5f925575c3903f3ae702f341f0122
hacking bay <57585542+Rushi136@users.noreply.github.com>
2020-10-02 10:44:34 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+' ' ' create by rishi' ' '
+
+import smtplib
+from os import system
+
+def main( ):
+print   '=======+====+======'
+print   '       create by Rishi          '
+print   '==============++==='
+print '                           ^
+print '     ||__|| ... || )) //\\ \\//
+print '     ||''''''''||  ...|| ))||   ||   ||
+print '                               '''''''''''Hacking,,, print            bay:::::::::
+
+main()
+print '[y] start the attack'
+print '[n] exit'
+option = input('==>')if option == y: file_path = raw_input('path of passwords file :')else: system('clear') exit()
+pass_file = open(file_path,'r')pass_list = pass_file.readlines()
